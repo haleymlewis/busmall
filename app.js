0017b4b00b4a7d3d227e9a4e52e7e579122aa6ca
Haley <haleymlewis@gmail.com>
2019-03-05 19:28:03 -0800

    saving edits from class tues 3/5

--- app.js
+++ app.js
@@ -5,104 +5,99 @@ var middlePic = document.getElementById('2nd-image');
 var rightPic = document.getElementById('3rd-image');
 
 var allProducts = [];
-var currentProducts = [];
+// var currentProducts = [];
 
-var clickCount = 0;
+// var clickCount = 0;
 
-function RandomProducts(name, displayName) {
+function RandomProducts(filepath,name) {
+  // this.displayName = displayName;
+  this.filepath =  filepath; //`assets/${name}`;
   this.name = name;
-  this.displayName = displayName;
-  this.filepath = `imgs/${name}`;
   this.clicks = 0;
   this.views = 0;
+  
   allProducts.push(this);
 }
 
-new RandomProducts('bag.jpg', 'R2D2 Bag');
-new RandomProducts('banana.jpg', 'Banana Slicer');
-new RandomProducts('bathroom.jpg', 'Bathroom Tablet Mount');
-new RandomProducts('boots.jpg', 'Toeless Rainboots');
-new RandomProducts('breakfast.jpg', 'Breakfast All-in-One');
-new RandomProducts('bubblegum.jpg', 'Meatball Bubblegum');
-new RandomProducts('chair.jpg', 'Impractical Chair');
-new RandomProducts('cthulu.jpg', 'Cthulu Action Figure');
-new RandomProducts('dog-duck.jpg', 'Pet Beak');
-new RandomProducts('dragon.jpg', 'Dragon Meat');
-new RandomProducts('pen.jpg', 'U-Pen-sils');
-new RandomProducts('pet-sweep.jpg', 'Pet Sweep');
-new RandomProducts('scissors.jpg', 'Pizza Scissors');
-new RandomProducts('shark.jpg', 'Shark Sleeping Bag');
-new RandomProducts('sweep.png', 'Baby Sweep');
-new RandomProducts('tauntaun.jpg', 'Tauntaun Sleeping Bag');
-new RandomProducts('unicorn.jpg', 'Unicorn Meat');
-new RandomProducts('usb.gif', 'Tenticle USB');
-new RandomProducts('water-can.jpg', 'Impractical Watering Can');
-new RandomProducts('wine-glass.jpg', 'Impractical Wine Glass');
+new RandomProducts('assets/bag.jpg', 'R2D2 Bag');
+new RandomProducts('assets/banana.jpg', 'Banana Slicer');
+new RandomProducts('assets/bathroom.jpg', 'Bathroom Tablet Mount');
+new RandomProducts('assets/boots.jpg', 'Toeless Rainboots');
+new RandomProducts('assets/breakfast.jpg', 'Breakfast All-in-One');
+new RandomProducts('assets/bubblegum.jpg', 'Meatball Bubblegum');
+new RandomProducts('assets/chair.jpg', 'Impractical Chair');
+new RandomProducts('assets/cthulu.jpg', 'Cthulu Action Figure');
+new RandomProducts('assets/dog-duck.jpg', 'Pet Beak');
+new RandomProducts('assets/dragon.jpg', 'Dragon Meat');
+new RandomProducts('assets/pen.jpg', 'U-Pen-sils');
+new RandomProducts('assets/pet-sweep.jpg', 'Pet Sweep');
+new RandomProducts('assets/scissors.jpg', 'Pizza Scissors');
+new RandomProducts('assets/shark.jpg', 'Shark Sleeping Bag');
+new RandomProducts('assets/sweep.png', 'Baby Sweep');
+new RandomProducts('assets/tauntaun.jpg', 'Tauntaun Sleeping Bag');
+new RandomProducts('assets/unicorn.jpg', 'Unicorn Meat');
+new RandomProducts('assets/usb.gif', 'Tenticle USB');
+new RandomProducts('assets/water-can.jpg', 'Impractical Watering Can');
+new RandomProducts('assets/wine-glass.jpg', 'Impractical Wine Glass');
 
 
-function chooseProducts() {
-  let currentPictures = [];
-  do {
-    do {
-      var randomNumber = Math.floor(Math.random() * allProducts.length);
-      var picture = allProducts[randomNumber];
-    } while (previousPictures.includes(picture) || currentPictures.includes(picture));
-    currentPictures.push(picture);
-  } while (currentPictures.length < 3);
+function showRandomProducts(){  
+  var random1 = Math.floor(Math.random() * allProducts.length);
 
-  return currentPictures;
-}
-
-var previousPictures = [];
-oneRound();
-
-function oneRound() {
-  var currentProducts = chooseProducts();
-  render(currentProducts);
-
-  for (var i = 0; i < 3; i++) {
-    currentProducts[i].views++;
+  leftPic.src = allProducts[random1].filepath;
+  leftPic.alt = allProducts[random1].name;
+  leftPic.title = allProducts[random1].name;
+  allProducts[random1].views++;
+  ///// To ensure all product images are different /////
+  var random2 = Math.floor(Math.random() * allProducts.length);
+  while (random2 === random1){
+    random2 = Math.floor(Math.random() * allProducts.length);
   }
-  previousPictures = currentProducts;
 
-  clickCount += 1;
+  middlePic.src = allProducts[random2].filepath;
+  middlePic.alt = allProducts[random2].name;
+  middlePic.title = allProducts[random2].name;
+  allProducts[random2].views++;
+  ///// To ensure all product images are different /////
+  var random3 = Math.floor(Math.random() * allProducts.length);
+  while (random3 === random1 || random3 === random2){
+    random3 = Math.floor(Math.random() * allProducts.length);
+  }
+
+  rightPic.src = allProducts[random3].filepath;
+  rightPic.alt = allProducts[random3].name;
+  rightPic.title = allProducts[random3].name;
+  allProducts[random3].views++;
 }
 
-function render(currentProducts){  
-  leftPic.src = currentProducts[random].filepath;
-  leftPic.alt = currentProducts[random].name;
-  leftPic.title = currentProducts[random].name;
+showRandomProducts();
 
-  middlePic.src = currentProducts[random].filepath;
-  middlePic.alt = currentProducts[random].name;
-  middlePic.title = currentProducts[random].name;
+leftPic.addEventListener('click', handleClick);
+middlePic.addEventListener('click', handleClick);
+rightPic.addEventListener('click', handleClick);
 
-  rightPic.src = currentProducts[random].filepath;
-  rightPic.alt = currentProducts[random].name;
-  rightPic.title = currentProducts[random].name;
-
-  leftPic.addEventListener('click', handleClick);
-  middlePic.addEventListener('click', handleClick);
-  rightPic.addEventListener('click', handleClick);
-}
-
-function handleClick(event) {
+function handleClick(event){
   console.log('target, ', event.target);
-  if (turnCount < 26) {
-    increaseClickCount(event.target.title);
-    oneRound();
-  } else if (turnCount === 26){
-    createList();
-  } else {
-    return;
-  }
+  showRandomProducts();
 }
 
-function increaseClickCount(title) {
-  for (var i = 0; i <allProducts.length; i++) {
-    if (allProducts[i].displayName === title) {
-      allProducts[i].clicks++;
-      break;
-    }
-  }
-}
+// function handleClick(event) {
+//   console.log('target, ', event.target);
+//   if (turnCount < 26) {
+//     increaseClickCount(event.target.title);
+//     oneRound();
+//   } else if (turnCount === 26){
+//     createList();
+//   } else {
+//     return;
+//   }
+// }
+
+// function increaseClickCount(title) {
+//   for (var i = 0; i <allProducts.length; i++) {
+//     if (allProducts[i].displayName === title) {
+//       allProducts[i].clicks++;
+//       break;
+//     }
+//   }
+// }
